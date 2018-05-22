57e44a41e1feef2300aa241f52ff31cec24362d0
michaelbragg <michael@michaelbragg.net>
2018-05-22 13:22:04 +0100

    Add deactivator class
    To be run on uninstall of plugin

---
+++ includes/class-deactivator.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.thoughtsideas.uk
+ * @since      0.1.0
+ *
+ * @package    TI/Projects
+ * @subpackage TI/Projects/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      0.1.0
+ * @package    TI/Projects
+ * @subpackage TI/Projects/includes
+ * @author     Thoughts & Ideas <hello@thoughtsideas.uk>
+ */
+class Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    0.1.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}

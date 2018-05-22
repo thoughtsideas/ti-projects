da5c3489586838cc5e0e1001b2c1438a7a454cbc
michaelbragg <michael@michaelbragg.net>
2018-05-22 13:21:18 +0100

    Add activator class
    To be run on instalation of plugin

---
+++ includes/class-activator.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.thoughtsideas.uk
+ * @since      1.0.0
+ *
+ * @package    TI/Projects
+ * @subpackage TI/Projects/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    TI/Projects
+ * @subpackage TI/Projects/includes
+ * @author     Thoughts & Ideas <hello@thoughtsideas.uk>
+ */
+class Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
